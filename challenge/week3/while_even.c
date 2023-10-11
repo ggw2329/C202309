a8ae8dfeb3b5fb3b337f4e151bb10492c8204b4e
ggw2329 <ggw2329@naver.com>
2023-10-11 15:04:08 +0900

    issues#1

--- challenge/week3/while_even.c
+++ challenge/week3/while_even.c
@@ -2,9 +2,9 @@
 
 int main() {
 	int i = 0;
-	while (i < 10) {
+	while (i < 10) {//i가 10보다 작을때 까지 1씩 추가
 		i++;
-		if (i % 2 == 0) {
+		if (i % 2 == 0) {//2로나눈 나머지가 0일때 계속진행
 			continue;
 		}
 		printf("Hello world %d\n", i);
