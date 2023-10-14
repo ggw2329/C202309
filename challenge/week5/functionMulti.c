b945a7393181afa69d5f7e92275554719efed5dc
ggw2329 <ggw2329@naver.com>
2023-10-14 14:14:52 +0900

    20231004 Feedback

--- challenge/week5/functionMulti.c
+++ challenge/week5/functionMulti.c
@@ -2,18 +2,20 @@
 
 long multiF(long value);
 
-void main() {
-	printf("1부터 2까지의 곱은 %d\n", multiF(2));
-	printf("1부터 3까지의 곱은 %d\n", multiF(3));
-	printf("1부터 5까지의 곱은 %d\n", multiF(5));
+int main() {
+	printf("1부터 2까지의 곱은 %ld\n", multiF(2));
+	printf("1부터 3까지의 곱은 %ld\n", multiF(3));
+	printf("1부터 5까지의 곱은 %ld\n", multiF(5));
+
+	return 0;
 }
 
-long multiF(long value) {
-	long i;
-	long multi = 1;
+long multiF(long value) {//multiF함수 설정
+	long i;//i 지정
+	long multi = 1;// multi=1 지정
 
 	for (i = 1; i <= value; i = i + 1) {
-		multi = multi * i;
+		multi = multi * i; // i가 value보다 작은 값까지 1씩늘리며 multi*i 반복
 	}
 	return multi;
 }
--- practice/week5/functionHap.c
+++ practice/week5/functionHap.c
@@ -2,20 +2,21 @@
 
 int hapf(int value);
 
-void main() {
+int main() {
 	printf("1부터 10까지의 합은 %d\n", hapf(10));
 	printf("1부터 100까지의 합은 %d\n", hapf(100));
 	printf("1부터 1000까지의 합은 %d\n", hapf(1000));
+	return 0;
 }
 
-int hapf(int value) {
+int hapf(int value) {// hapf함수 설정
 	int i;
 	int hap = 0;
 
 	i = 1;
 	while (i <= value) {
 			hap = hap + i;
-			i++;
+			i++; // i가 value보다 작거나 같을때까지 hap+i후 i를 1씩 늘림
 	}
 	return hap;
 }
